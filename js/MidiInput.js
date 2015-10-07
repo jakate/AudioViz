bf2c1b5f09c348f172846087db10344f53bd3c44
Arto Chydenius <arto.chydenius@futurice.com>
2015-10-07 21:32:57 +0300

    Read note on and control change events from MIDI input

--- js/MidiInput.js
+++ js/MidiInput.js
@@ -3,7 +3,7 @@ var MidiInput = function() {
     this.visualizer = visualizer;
 
     if (navigator.requestMIDIAccess) {
-        navigator.requestMIDIAccess().then(onMidiSuccess, onMidiReject);
+        navigator.requestMIDIAccess().then(onMidiSuccess.bind(this), onMidiReject);
     } else {
       console.log('No MIDI support: navigator.requestMIDIAccess is not defined');
     }
@@ -14,6 +14,42 @@ var MidiInput = function() {
   }
 
   function onMidiSuccess(midi) {
-    console.log('MIDI init ok!');
+    if (midi.inputs.size > 0) {
+      // Get first MIDI device
+      var input = midi.inputs.values().next().value;
+      console.log('MIDI device found: ' + input.name);
+
+      // Set MIDI message handler
+      input.onmidimessage = messageHandler.bind(this);
+    } else {
+      console.log('No MIDI input devices available');
+    }
+  }
+
+  function messageHandler(event) {
+    var data = {
+      status: event.data[0] & 0xf0,
+      data: [
+        event.data[1],
+        event.data[2]
+      ]
+    };
+
+    handleInput(data);
+  }
+
+  function handleInput(data) {
+    // Note on event
+    if (data.status === 144) {
+      var note = data.data[0];
+      var velocity = data.data[1];
+      console.log('Note on: ' + note + ', ' + velocity);
+
+    // Control change event
+    } else if (data.status === 176) {
+      var index = data.data[0];
+      var value = data.data[1];
+      console.log('Control change: ' + index + ', ' + value);
+    }
   }
 };
