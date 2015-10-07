965a4613e25dfeaeebc79962bcb05ce54a8d64f0
Arto Chydenius <arto.chydenius@futurice.com>
2015-10-07 17:51:16 +0300

    Move tempo calculation to a separate function and update key commands to match README

--- js/KeyboardInput.js
+++ js/KeyboardInput.js
@@ -8,6 +8,16 @@ var KeyboardInput = function() {
     window.addEventListener('keydown', dealWithKeyboard.bind(this), false);
   }
 
+  function getBpm() {
+    var tmpTime = new Date().getTime();
+    tapTempo = tmpTime - tapTempoTs;
+    tapTempoTs = tmpTime;
+
+    var bpm = Math.round(1 / (tapTempo / 1000) * 60);
+
+    return bpm;
+  }
+
   function dealWithKeyboard(e) {
     var key = String.fromCharCode(e.keyCode);
 
@@ -16,38 +26,35 @@ var KeyboardInput = function() {
       case '0':
         this.visualizer.toggleAutoPlay();
         break;
-      // Number 1
-      case '1':
-        var tmpTime = new Date().getTime();
-        tapTempo = tmpTime - tapTempoTs;
-        tapTempoTs = tmpTime;
-        var bpm = Math.round(1 / (tapTempo / 1000) * 60);
-
-        this.visualizer.setBpm(bpm);
-        break;
       // Number 2
-      case '2':
+      case '1':
         this.visualizer.toggleMode('background');
         break;
       // Number 3
-      case '3':
+      case '2':
         this.visualizer.toggleMode('blocks');
         break;
       // Number 4
-      case '4':
+      case '3':
         this.visualizer.toggleMode('circle');
         break;
       // Number 5
-      case '5':
+      case '4':
         this.visualizer.toggleMode('flower');
         break;
       // Number 6
-      case '6':
+      case '5':
         this.visualizer.toggleMode('smoke');
         break;
       // Number 7
-      case '7':
+      case '6':
         this.visualizer.triggerBlast();
+        break;
+
+      // --- Tap tempo ---
+      case 'T':
+        var bpm = getBpm();
+        this.visualizer.setBpm(bpm);
         break;
 
       // --- Colors ---
