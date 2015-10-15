40ad23f0d3a6ec9cbca9bd9c3972860152151daa
Jaakko Tepponen <jaakko.tepponen@futurice.com>
2015-10-15 20:15:45 +0300

    updated paths

--- server.py
+++ server.py
@@ -5,5 +5,5 @@ import ssl
 
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/Users/jtep/Code/own/audioviz/cert.pem', keyfile='/Users/jtep/Code/own/audioviz/key.pem',server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./cert.pem', keyfile='./key.pem',server_side=True)
 httpd.serve_forever()
