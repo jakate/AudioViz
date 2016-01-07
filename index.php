ea2cf2490c4f099fc3b31e8405a3852a0e7c3ac0
Jaakko Tepponen <jaakko.tepponen@futurice.com>
2016-01-07 13:44:50 +0200

    php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <title>Music Visualization</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--div id="formHolder">
+            <form id="songForm">
+                <div class="group">
+                    <input type="text" placeholder="Search" name="song" value=""/>
+                    <p class="info">Search 30s preview from Spotify</p>
+                </div>
+                <div class="group">
+                    <input type="submit" value="Load song">
+                </div>
+            </form>
+            <form id="micForm">
+                <div class="group">
+                    <input type="submit" value="Use Mic">
+                </div>
+            </form>
+        </div-->
+
+        <script src="js/lib/lodash.min.js"></script>
+        <script src="js/lib/spotify.js"></script>
+        <script src="js/lib/datgui.js"></script>
+        <script src="js/lib/three.js"></script>
+        <script src="js/lib/ShaderParticles.min.js"></script>
+        <script src="js/lib/FlyControls.js"></script>
+
+        <script src="js/JEvents.js"></script>
+        <script src="js/Chilicorn.js"></script>
+        <script src="js/Settings.js"></script>
+        <script src="js/ParticleStream.js"></script>
+        <script src="js/ParticleStreams.js"></script>
+        <script src="js/VideoPlane.js"></script>
+        <script src="js/Smoke.js"></script>
+        <script src="js/Background.js"></script>
+        <script src="js/Blocks.js"></script>
+        <script src="js/AudioDrawer.js"></script>
+        <script src="js/AudioLoader.js"></script>
+        <script src="js/AudioPlayer.js"></script>
+        <script src="js/KeyboardInput.js"></script>
+        <script src="js/MidiInput.js"></script>
+        <script src="js/AudioVisualizer.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
